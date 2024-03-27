bca6ea1a916222d804b6ae10463a6a7774f8e308
OXsatoshi <afia.nabil24@gmail.com>
2024-03-28 00:42:36 +0100

    solving 4 challeng

--- 03_reverseString/solution/reverseString-solution.js
+++ 03_reverseString/solution/reverseString-solution.js
@@ -1,5 +1,8 @@
 const reverseString = function (string) {
+  console.log(string.split(""));
+  console.log(string.split("").reverse());
+  console.log(string.split("").reverse().join(""));
   return string.split("").reverse().join("");
 };
-
+console.log(reverseString("hello world"));
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,e) {
+    let arrayExcludingValueGived = array;
+    for(let i = 1;i<arguments.length;i++){
+        arrayExcludingValueGived = removeOneItem(arrayExcludingValueGived,arguments[i]);
+    }
+    return arrayExcludingValueGived;
 };
-
+function removeOneItem(array,item){
+    return array.filter( currentValue=>currentValue!==item);
+}
 // Do not edit below this line
 module.exports = removeFromArray;
