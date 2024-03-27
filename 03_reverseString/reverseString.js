f34c247ad0b573d3149070325d82fdf0b274bd79
OXsatoshi <afia.nabil24@gmail.com>
2024-03-28 00:06:44 +0100

    solving reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    let arrayOfString = string.split(" ");
+    let reversedString="";
+    for(let i=arrayOfString.length-1;i>=0;i--){
+        reversedString+=reverseSingleString(arrayOfString[i]);
+        reversedString+=" ";
+    }
+    return reversedString.trimEnd();   
+}
+function reverseSingleString(string){
+    let reversedString="";
+    for(let i = string.length-1 ; i>=0;i--){
+  
+        reversedString+=string.charAt(i);
+        
+    }
+    
+    return reversedString;
+}
+console.log(reverseSingleString("heloo"));
 // Do not edit below this line
 module.exports = reverseString;
