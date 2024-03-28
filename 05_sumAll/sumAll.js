b0a70b70f85e0620327fa75d76cfa4e768b3f408
OXsatoshi <afia.nabil24@gmail.com>
2024-03-28 01:07:20 +0100

    solving challeng number 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(start,end) {
+    let sum = 0 ;
+    if(typeof start !== "number" || typeof end !== "number") return "ERROR";
+    if(start < 0 || end < 0) return "ERROR";
+    else {
+     let minIndex = min(start,end);
+     let maxIndex = max(start,end);
+     for(let i = minIndex;i<=maxIndex;i++){
+        sum+=i;
+     }
+     return sum;
+    }
+}
+function min(a,b){
+    if(a < b) return a;
+    if(b < a) return b;
+}
+function max(a,b){
+    if(a > b) return a;
+    if(b > a) return b;
+}
 // Do not edit below this line
 module.exports = sumAll;
